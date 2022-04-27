643f1b837ebb8fbb4b0ab90a2677b40781df025a
jmakhmudov <makhmudov@tuta.io>
2022-04-27 21:56:55 +0500

    created class room

--- main.cpp
+++ main.cpp
@@ -3,10 +3,30 @@
 #include <fstream>
 using namespace std;
 
-class Room
-{
-public:
-    int room_id;
+class Room{
+private:
+    int room_id, beds, resident_q;
+    string renter, date_of_rent, date_of_expire;
+    float price_per_night;
+public: 
+    void setRoom_id(int r){ room_id = r;}
+    void setBeds(int b){ beds = b;}    
+    void setResident_q(int r){ resident_q = r;}
+    void setRenter(string a){ renter = a;}
+    void setDateRent(string d){ date_of_rent = d;}
+    void setDateExpire(string d){ date_of_expire = d;}
+    void setPrice(float p){ price_per_night = p;}
+
+    int getRoom_id(){ return room_id;}
+    int getBeds(){ return beds;}    
+    int getResident_q(){ return resident_q;}
+    string getRenter(){ return renter;}
+    string getDateRent(){ return date_of_rent;}
+    string getDateExpire(){ return date_of_expire;}
+    float getPrice(){ return price_per_night;}
+
+
+
 
 
 };
