03190c0f1243af80df6d08384ec7370708a2af74
jmakhmudov <makhmudov@tuta.io>
2022-04-28 12:11:44 +0500

    .

--- main.cpp
+++ main.cpp
@@ -6,13 +6,14 @@ using namespace std;
 class Room{
 private:
     int room_id, beds, resident_q;
-    string renter, date_of_rent, date_of_expire;
+    string renter_first_name, renter_last_name, date_of_rent, date_of_expire;
     float price_per_night;
 public: 
     void setRoom_id(int r){ room_id = r;}
     void setBeds(int b){ beds = b;}    
     void setResident_q(int r){ resident_q = r;}
-    void setRenter(string a){ renter = a;}
+    void setRenterName(string a){ renter_first_name = a;}
+    void setRenterLast(string a){ renter_last_name = a;}
     void setDateRent(string d){ date_of_rent = d;}
     void setDateExpire(string d){ date_of_expire = d;}
     void setPrice(float p){ price_per_night = p;}
@@ -20,19 +21,21 @@ public:
     int getRoom_id(){ return room_id;}
     int getBeds(){ return beds;}    
     int getResident_q(){ return resident_q;}
-    string getRenter(){ return renter;}
+    string getRenterName(){ return renter_first_name;}
+    
     string getDateRent(){ return date_of_rent;}
     string getDateExpire(){ return date_of_expire;}
     float getPrice(){ return price_per_night;}
-
-
-
-
-
 };
 
-
-
+void registration(){
+    string s;
+    int i;
+    float f;
+    cout << "\nEnter the name of renter : ";
+    cin >> s;
+    cout << ""
+}
 
 int main(){
     
